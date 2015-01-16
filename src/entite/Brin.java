a1ee5279d05d870ff33f7a6dde1a85c05e0b1b3c
Verbaere Thibaud <verbaere@a12p9.fil.univ-lille1.fr>
2015-01-16 11:42:42 +0100

    MAJ du fichier Brin.java

--- src/entite/Brin.java
+++ src/entite/Brin.java
@@ -1,13 +1,47 @@
 package entite;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Brin {
+	
+	private String sequence;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Brin(String filename) {
+		this.sequence = Fasta2String(filename);
 	}
 
+	private String Fasta2String(String filename) {
+		String ligne = "";
+		try{
+			InputStream ips=new FileInputStream(filename); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			/* lecture de  l'entête = inutile */
+			ligne=br.readLine();
+			/* lecture de la sequence ADN qui est sur une seule ligne */
+			ligne=br.readLine();
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		return ligne;
+	}
+
+
+	public int SizeofSequence() {
+		return this.sequence.length();
+	}
+	
+	public String getSequence() {
+		return sequence;
+	}
+	
+	
+
 }
+
+	
