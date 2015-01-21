a872490cdc9e729f0ef60c03fea723a3d22ea08d
Jean-Serge <j.monbailly@orange.Fr>
2015-01-21 11:25:52 +0100

    Création d'un fichier Tools et implémentation dans celui-ci d'une fonction permet de savoir si une chaine est un nombre.

---
+++ src/Tools.java
@@ -0,0 +1,23 @@
+/**
+ * Classe proposant des fonctions statiques utilisable 
+ * dans le projet.
+ * @author monbailly
+ *
+ */
+public class Tools {
+
+	/**
+	 * Cette fonction permet de savoir si une chaîne de
+	 * caractères est un nombre.
+	 * @param s la chaîne à observer
+	 * @return true si la chaine est un nombre, false sinon
+	 */
+	public static boolean isNumber(String s){
+		// Vérifie si chaque caractère de la chaîne est un digit
+		for(int i = 0 ; i < s.length() ; i++){
+			if(!Character.isDigit(s.charAt(i)))
+				return false;
+		}
+		return true;
+	}
+}
