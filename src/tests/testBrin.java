88d2b295beea80be3f23a9976de76787451561a4
Verbaere Thibaud <verbaere@a10p25.fil.univ-lille1.fr>
2015-01-21 11:07:33 +0100

    MAJ testBrin.java (Utilisation de Junit)

--- src/tests/testBrin.java
+++ src/tests/testBrin.java
@@ -1,5 +1,7 @@
 package tests;
 
+import junit.framework.*;
+import org.junit.Test;
 import entite.Brin;
 
 /**
@@ -8,28 +10,33 @@ import entite.Brin;
 
 public class testBrin {
 	
-	public static void main(String[] args) {
-		// On construit les brins pour les tests
-		Brin brintest1 = new Brin("donnees/test1.fasta");
-		Brin brintest2 = new Brin("donnees/test2.fasta");
-		// On applique la fonction SizeofSequence sur les brins
-		int tailleB1 = brintest1.SizeofSequence();
-		int tailleB2 = brintest2.SizeofSequence();
+	// On construit les brins pour les tests
+	Brin brintest1 = new Brin("donnees/test1.fasta");
+	Brin brintest2 = new Brin("donnees/test2.fasta");
+	
+	@Test
+	public void TestConstructeur() {
 		// On applique la fonction getSequence sur les brins
 		String SeqB1 = brintest1.getSequence();
 		String SeqB2 = brintest2.getSequence();
 		
-		// On vérifie les résultats :
-		if (8 != tailleB1) 
-            System.out.println("KO : Le brin d'ADN 'test1.fasta' doit avoir une taille de 8 (GATACACA).");
-		if (12 != tailleB2)
-			 System.out.println("KO : Le brin d'ADN 'test2.fasta' doit avoir une taille de 12 (TATATATATATA).");
-		if (!"GATACACA".equals(SeqB1))
-			 System.out.println("KO : Le brin d'ADN 'test1.fasta' doit être 'GATACACA'.");
-		if (!"TATATATATATA".equals(SeqB2))
-			 System.out.println("KO : Le brin d'ADN 'test2.fasta' doit être 'TATATATATATA'.");
+		// Tests :
+		Assert.assertEquals("Le brin d'ADN 'test1.fasta' doit être 'GATACACA'.","GATACACA",SeqB1);
 		
-		System.out.println("tests OK");
+		Assert.assertEquals("Le brin d'ADN 'test2.fasta' doit être 'TATATATATATA'.","TATATATATATA",SeqB2);
+	}
+	
+	@Test 
+	public void TestTaille() {
+		// On applique la fonction SizeofSequence sur les brins
+		int tailleB1 = brintest1.SizeofSequence();
+		int tailleB2 = brintest2.SizeofSequence();
+		
+		// Tests :
+		Assert.assertEquals("Le brin d'ADN 'test1.fasta' doit avoir une taille de 8 (GATACACA).",8,tailleB1);
+		
+		Assert.assertEquals("Le brin d'ADN 'test2.fasta' doit avoir une taille de 12 (TATATATATATA).",12,tailleB2);
+
 		
 	}
 	
