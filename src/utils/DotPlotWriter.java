537ae230b927343e2d2c5b9e0273b4310bc064a2
Verbaere Thibaud <verbaere@a10p24.fil.univ-lille1.fr>
2015-01-23 13:12:40 +0100

    classe dotplot

--- src/utils/DotPlotWriter.java
+++ src/utils/DotPlotWriter.java
@@ -5,7 +5,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
+
+import recherche.Recherche;
 
 /**
  * @author monbailly
@@ -15,21 +16,26 @@ import java.util.Map;
 public class DotPlotWriter {
 	
 	private PrintWriter out;
-	private Map<String, List<Integer>> results;
-	private String sequence;
+	private PrintWriter plot;
+	private Recherche recherche;
+	private String path;
 	
 	
 	/**
 	 * @param file
 	 * @param result
 	 */
-	public DotPlotWriter(String file,Map<String, List<Integer>> result,String sequence) {
+	public DotPlotWriter(String file, Recherche r) {
 
 		try {
-			File fichier = new File(file) ;
-			this.out = new PrintWriter(new FileWriter(fichier));	
-			this.results = result;
-			this.sequence = sequence;
+			File fichier = new File(file+".txt") ;
+			this.out = new PrintWriter(new FileWriter(fichier));
+			
+			File fichier2 = new File(file+".plot") ;
+			this.plot = new PrintWriter(new FileWriter(fichier2));
+			
+			this.path = file;
+			this.recherche = r;
 		}
 		catch (IOException e) {
 			e.printStackTrace();
@@ -41,15 +47,29 @@ public class DotPlotWriter {
 	 * 
 	 */
 	public void printResults() {
-		for (String s : this.results.keySet()) {
-			List<Integer> list = this.results.get(s);
+		for (String s : this.recherche.getResultats().keySet()) {
+			List<Integer> list = this.recherche.getResultats().get(s);
 			for (Integer i : list) {
 				for (Integer j : list) {
-					out.println(i+" "+j);
+					if(i == j)
+						out.println(i+" "+j);
 				}
 			}
 		}
+		this.printPlot();
 		out.close() ;
+	}
+	
+	public void printPlot() {
+		plot.println("set term png;");
+		
+		plot.println("set xrange [0:"+(this.recherche.getSequence().length()-1)+"];");
+		plot.println("set yrange[0:"+(this.recherche.getSequence().length()-1)+"];");
+		
+		plot.println("set output '"+this.path+"';");
+		plot.println("plot '"+this.path+".txt' using 1:2 title 'Recherche pour N="+this.recherche.getN()+"' with points;");
+		
+		plot.close() ;
 	}
 	
 }
