b63ec230a5fcecc5c044a9e5d99b9ce06fca8fce
JS <jean-serge.monbailly@etudiant.univ-lille1.fr>
2015-03-03 14:58:54 +0100

    Ajout de l'exécutable pour le programme.

--- rechercheMotif.bash
+++ rechercheMotif.bash
@@ -4,10 +4,53 @@
 ### .fasta, il est paramétrable avec la taille des motifs à rechercher ainsi
 ### que les éventuelles options de la recherche.
 
+usage()
+{
+    echo
+    echo "Pour utiliser ce programme vous devez spécifier les paramètres suivants :"
+    echo "  - le chemin vers le fichier .fasta à lire"
+    echo "  - un entier correspondant à la taille des mots à rechercher"
+    echo
+    echo "Vous pouvez également spécifier les options de recherche (facultatives) suivantes :"
+    echo "  - -r pour rechercher les mot de taille N et leur inverse"
+    echo "  - -c pour rechercher les mot de taille N et leur complémentaire"
+    echo "  - -rc pour rechercher les mot de taille N et leur inverse-complémentaire"
+    echo
+    echo "Ce programme compilera les sources et exécutera l'application Java avec les paramètres spécifiés."
+    echo "Cette application créera ensuite un fichier .plot et .txt qui serviront à générer le plot final."
+}
+
+
+if [ ! -d bin ];
+then
+	mkdir bin
+fi
+
+javac -d bin src/*/*.java
+
+
 option=''
-for (( i=0 ; i<$# ; i++ )) 
+for i in "$@" 
 do
-	$option=($option + $$i)
+	option=$option" "$i
 done
 
-echo $option
+java -cp bin main.Main $option
+
+
+if expr $? = 0 >/dev/null 
+then 
+	if [ -f 'resultats.plot' ];
+	then
+	    gnuplot resultats.plot 2>/dev/null
+	fi
+
+	if expr $? != 0 >/dev/null 
+	then 
+	    echo "gnuplot doit être installé pour pouvoir générer le resultat."
+	else
+	    echo "Pour voir les résultats, visionnez le fichier resultats."
+	fi
+else
+    usage
+fi
